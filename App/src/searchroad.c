048548e504efd69ec2eb90b2b0dd8fa4ebd48884
thinkpade450 <1464089197@qq.com>
2018-04-21 13:02:49 +0800

    改进了识别圆环和十字
    改进了识别圆环和十字

--- App/src/searchroad.c
+++ App/src/searchroad.c
@@ -1,3 +1,6 @@
+/*
+赛道为1，黑线为0
+*/
 #include "searchroad.h"
 #include "common.h"
 #include "include.h"
@@ -6,12 +9,15 @@
 float average_offset[11] = { 0,0,0,0,0,0,0,0,0,0,0 };                 //偏差度，为平均偏差度
 uint8 imgbuff[CAMERA_SIZE];                                         //定义存储接收图像的数组
 uint8 img[CAMERA_H][CAMERA_W];
+
 int iscross_flag = 0;            //判断是否是十字道的标志 0表示没有进入1表示初步判断并开始计数距离
 int iscross_count = 0;           //十字道距离计数
-int isisland_flag = 0;           //是否是环岛的判断 0表示没有进入1表示初步判断并开始计数距离
-int isisland_count = 0;          //环岛距离计数
-//int rode_wide[CAMERA_H] = {};    //记录赛道宽度的队列
 
+int isisland_flag = 0;           //是否是环岛的判断 0表示没有进入1表示初步判断并开始计数距离
+int isisland_flag1 = 0;
+int isisland_count = 0;          //环岛距离计数
+
+float temp_s = 0;
 
 
 
@@ -25,7 +31,6 @@ void Search_line_init()
 
 void Search_line()
 {
-	//int black_temp[CAMERA_H];
 	int left_black[CAMERA_H];                          //左边黑线数组
 	int left_black_before = CAMERA_W / 2;              //上一次左边扫描到的黑线位置
 	int left_next_line = 0;                            //判断扫描是否进入了新的一行
@@ -45,6 +50,7 @@ void Search_line()
 	int count = 0;
 	int i = 0;
 	int j = 0;
+	int m = 0;
 
 	left_black_before = CAMERA_W / 2;
 	right_black_before = CAMERA_W / 2;
@@ -74,21 +80,44 @@ void Search_line()
 		}
 		if ((0 == jw_left) && (0 == left_find_flag))          //左边找到最后一个像素任然未找到符合黑线条件的像素，则认为第一个为黑线
 		{
-			if (left_black[jh + 1] != -1 && jh < 50)                        //补扫，判断是否是圆环
+			if (left_black[jh + 1] != -1 && jh < 50 && 1 == img[jh + 1][left_black[jh + 1] + 3])                       //补扫，判断是否是圆环
 			{
-				for (i = 2; i < 8; i++)
+				if (left_black[jh + 1] - 2 >= 0)
 				{
-					j = left_black[jh + i] - 1;
-					while (img[jh + i][j] != 1 && j > 0)
+					if (1 == img[jh + 1][left_black[jh + 1] - 2])
 					{
-						j--;
+						for (i = 2; i < 6; i++)
+						{
+							j = left_black[jh + i] - 1;
+							while (img[jh + i][j] != 1 && j > 0)
+							{
+								j--;
+							}
+							if (0 == j) break;
+						}
+						if (5 == i)
+						{
+							isisland_flag1++;
+							//isisland_flag = 1;
+						}
 					}
-					if (0 == j) break;
 				}
-				if (7 == i)
+				else
 				{
-					isisland_flag++;
-					//isisland_flag = 1;
+					for (i = 2; i < 6; i++)
+					{
+						j = left_black[jh + i] - 1;
+						while (img[jh + i][j] != 1 && j > 0)
+						{
+							j--;
+						}
+						if (0 == j) break;
+					}
+					if (5 == i)
+					{
+						isisland_flag1++;
+						//isisland_flag = 1;
+					}
 				}
 			}
 			left_find_flag = 1;
@@ -116,21 +145,42 @@ void Search_line()
 		}
 		if (jw_right == (CAMERA_W - 1) && (0 == right_find_flag))            //右边黑点未找到
 		{
-			if (right_black[jh + 1] != -1 && jh < 50)                        //补扫，判断是否是圆环
+			if (right_black[jh + 1] != -1 && jh < 50 && 1 == img[jh + 1][right_black[jh + 1] - 3])                        //补扫，判断是否是圆环
 			{
-				for (i = 2; i < 8; i++)
+				if (right_black[jh + i] + 2 <= CAMERA_W - 1)
 				{
-					j = right_black[jh + i] + 1;
-					while (img[jh + i][j] != 1 && j < CAMERA_W - 1)
+					if(1 == img[jh + 1][right_black[jh + i] + 2])
+					for (i = 2; i < 6; i++)
 					{
-						j++;
+						j = right_black[jh + i] + 1;
+						while (img[jh + i][j] != 1 && j < CAMERA_W - 1)
+						{
+							j++;
+						}
+						if ((CAMERA_W - 1) == j) break;
 					}
-					if ((CAMERA_W - 1) == j) break;
+					if (5 == i)
+					{
+						isisland_flag1++;
+						//isisland_flag = 1;
+					}
 				}
-				if (7 == i)
+				else
 				{
-					isisland_flag++;
-					//isisland_flag = 1;
+					for (i = 2; i < 6; i++)
+					{
+						j = right_black[jh + i] + 1;
+						while (img[jh + i][j] != 1 && j < CAMERA_W - 1)
+						{
+							j++;
+						}
+						if ((CAMERA_W - 1) == j) break;
+					}
+					if (5 == i)
+					{
+						isisland_flag1++;
+						//isisland_flag = 1;
+					}
 				}
 			}
 			right_find_flag = 1;
@@ -150,32 +200,10 @@ void Search_line()
 
 
 	/*----------------根据图像判断路况--------------------*/
-	//jh = LINE_NUM - 1;
-	//while (jh >= 0)   //计算路宽
-	//{
-	//	if (left_black[jh] != -1 && right_black[jh] != -1)
-	//	{
-	//		rode_wide[jh] = right_black[jh] - left_black[jh];
-	//	}
-	//	else if (left_black[jh] != -1 && -1 == right_black[jh])
-	//	{
-	//		rode_wide[jh] = CAMERA_W - left_black[jh];
-	//	}
-	//	else if (right_black[jh] != -1 && -1 == left_black[jh])
-	//	{
-	//		rode_wide[jh] = right_black[jh];
-	//	}
-	//	else
-	//	{
-	//		rode_wide[jh] = CAMERA_W;
-	//	}
-	//	j--;
-	//}
-
-	jh = LINE_NUM - 8;
-	while (jh >= 8)
+	jh = LINE_NUM - 5;
+	while (jh >= 4)
 	{
-		if ((left_black[jh + 3] - left_black[jh + 4] > 0 &&
+		if ((left_black[jh + 3] - left_black[jh + 4] > 0 &&           //判断是否是圆环
 			left_black[jh + 2] - left_black[jh + 3] > 0 &&
 			left_black[jh + 1] - left_black[jh + 2] > 0 &&
 			left_black[jh + 0] - left_black[jh + 1] > 0 &&
@@ -186,79 +214,92 @@ void Search_line()
 			)
 		{
 			
-			if (left_black[jh + 0] - left_black[jh + 4] > 6 && left_black[jh + 0] - left_black[jh - 4] > 6)//突出点够尖锐
+			if (left_black[jh + 0] - left_black[jh + 4] > 6 && left_black[jh + 0] - left_black[jh - 4] > 6 && 1 == img[jh][left_black[jh] + 3])//突出点够尖锐
 			{
 				isisland_flag++;
+				LCD_numf(tem_site_str[2], jh, GREEN, BLUE);
 				//isisland_flag = 1;  
 			}
-			else if (abs((left_black[jh - 4] - left_black[jh])*(left_black[jh + 4] - left_black[jh]) - 16) < 4)  //突出为直角
+			
+		}
+
+		j = 0;
+		m = 0;
+		if (-1 == left_black[jh] && -1 == right_black[jh])   //判断是否是十字
+		{
+			for (i = 1; i < 20; i++)
+			{
+				if (jh + i > LINE_NUM - 1) break;
+				if (left_black[jh + i] != -1)
+				{
+					if (left_black[jh + i] > 1)
+					{
+						j = 1;
+					}
+					break;
+				}
+			}
+			for (i = 1; i < 20; i++)
+			{
+				if (jh + i > LINE_NUM - 1) break;
+				if (right_black[jh + i] != -1)
+				{
+					if (right_black[jh + i] < CAMERA_W - 2)
+					{
+						m = 1;
+					}
+					break;
+				}
+			}
+			if (1 == j && 1 == m)
 			{
 				iscross_flag++;
-				//iscross_flag = 1;
 			}
 		}
-		//else if (
-		//	    right_black[jh + 3] - right_black[jh + 4] < 0 &&
-		//		right_black[jh + 2] - right_black[jh + 3] < 0 &&
-		//		right_black[jh + 1] - right_black[jh + 2] < 0 &&
-		//		right_black[jh + 0] - right_black[jh + 1] < 0 &&
-		//		right_black[jh - 1] - right_black[jh - 0]>0 &&
-		//		right_black[jh - 2] - right_black[jh - 1]>0 &&
-		//		right_black[jh - 3] - right_black[jh - 2]>0 &&
-		//		right_black[jh - 4] - right_black[jh - 3]>0
-		//	    )
-		//{
-		//	if (abs((right_black[jh - 4] - right_black[jh])*(right_black[jh + 4] - right_black[jh]) - 16) < 4)//突出为直角
-		//	{
-		//		iscross_flag++;
-		//		//iscross_flag = 1; 
-		//	}
-		//		
-		//}
-		j--;
+		jh--;
 	}
 
 
 	/*---------------------中心点的计算------------------------*/
-	//jh = LINE_NUM - 1;
-	//while (jh >= 0)
-	//{
-	//	//if (-1 == left_black[jh]) left_black[jh] = 0;
-	//	//if (-1 == right_black[jh]) right_black[jh] = LINE_NUM - 1;
+	jh = LINE_NUM - 1;
+	while (jh >= 0)
+	{
+		//if (-1 == left_black[jh]) left_black[jh] = 0;
+		//if (-1 == right_black[jh]) right_black[jh] = LINE_NUM - 1;
 
-	//	if (jh == (LINE_NUM-1))
-	//		middleline[jh] = (left_black[jh] + right_black[jh]) / 2;
-	//	else
-	//	{
-	//		if (-1 == left_black[jh] || -1 == right_black[jh])
-	//		{
-	//			if (-1 == right_black[jh]) middleline[jh] = left_black[jh] + 36 / (1 + (60 - jh)*TRAPEZOID_CORRECT / 40);
-	//			else middleline[jh] = right_black[jh] - 36 / (1 + (60 - jh)*TRAPEZOID_CORRECT / 40);
-	//		}
-	//		else
-	//		{
-	//			middleline[jh] = (left_black[jh] + right_black[jh]) / 2;
-	//		}
-	//			
-	//		/*----------------中心点无效或者扫描停止的判断------------------*/
-	//		//if (abs(middleline[jh + 1] - middleline[jh]) > DEGREE_OF_ERROR)     middleline[jh] = -1;   //若果斜率过大（急转弯）或误差较大中心点无效
+		if (jh == (LINE_NUM-1))
+			middleline[jh] = (left_black[jh] + right_black[jh]) / 2;
+		else
+		{
+			if (-1 == left_black[jh] || -1 == right_black[jh])
+			{
+				if (-1 == right_black[jh]) middleline[jh] = left_black[jh] + 36 / (1 + (60 - jh)*TRAPEZOID_CORRECT / 40);
+				else middleline[jh] = right_black[jh] - 36 / (1 + (60 - jh)*TRAPEZOID_CORRECT / 40);
+			}
+			else
+			{
+				middleline[jh] = (left_black[jh] + right_black[jh]) / 2;
+			}
+				
+			/*----------------中心点无效或者扫描停止的判断------------------*/
+			//if (abs(middleline[jh + 1] - middleline[jh]) > DEGREE_OF_ERROR)     middleline[jh] = -1;   //若果斜率过大（急转弯）或误差较大中心点无效
 
-	//		//else if (0 == img[jh][middleline[jh]] && (jh < 30))
-	//		//{
-	//		//	middleline[jh] = -2;  //-2代表之后扫描作废
-	//		//	break;
-	//		//}
-	//		//else if (abs(middleline[jh + 1] - middleline[jh]) > REFLECT_LIGHT_ERROR && (jh < REFLECT_LIGHT_AREA))  //反光偏差过大
-	//		//{
-	//		//	break;
-	//		//}
-	//		//if ((middleline[jh]< CLOSE_TO_EDGE) || (middleline[jh]>(CAMERA_W - CLOSE_TO_EDGE))) //黑线接近图像边缘,退出扫描
-	//		//{
-	//		//	break;
-	//		//}
-	//	}
-	//	jh--;
-	//}
+			//else if (0 == img[jh][middleline[jh]] && (jh < 30))
+			//{
+			//	middleline[jh] = -2;  //-2代表之后扫描作废
+			//	break;
+			//}
+			//else if (abs(middleline[jh + 1] - middleline[jh]) > REFLECT_LIGHT_ERROR && (jh < REFLECT_LIGHT_AREA))  //反光偏差过大
+			//{
+			//	break;
+			//}
+			//if ((middleline[jh]< CLOSE_TO_EDGE) || (middleline[jh]>(CAMERA_W - CLOSE_TO_EDGE))) //黑线接近图像边缘,退出扫描
+			//{
+			//	break;
+			//}
+		}
+		jh--;
+	}
 		
 
 	/*--------------------------各种路况下的预备处理-------------------------*/
@@ -273,29 +314,29 @@ void Search_line()
 
 
     /*------------------------偏差度计算及中线展现-----------------------------*/
-	//for (i = LINE_NUM - 1; i >= 0; i--)
-	//{
-	//	if (-2 == middleline[i])
-	//		break;
-	//	else if (-1 == middleline[i]) {}
-	//	else
-	//	{
-	//		offset = offset + ((float)(middleline[i] - CAMERA_W / 2)*(1 + (60 - i)*TRAPEZOID_CORRECT / 40));          //offset是补偿，用来描述整体赛道的偏向,<0偏左
-	//		count++;
-	//		if (middleline[i] > CAMERA_W - 1)middleline[i] = CAMERA_W - 1;
-	//		if (middleline[i] < 0)middleline[i] = 0;
-	//		img[i][middleline[i]] = !img[i][middleline[i]];
-	//	}
-	//}
+	for (i = LINE_NUM - 1; i >= 0; i--)
+	{
+		if (-2 == middleline[i])
+			break;
+		else if (-1 == middleline[i]) {}
+		else
+		{
+			offset = offset + ((float)(middleline[i] - CAMERA_W / 2)*(1 + (60 - i)*TRAPEZOID_CORRECT / 40));          //offset是补偿，用来描述整体赛道的偏向,<0偏左
+			count++;
+			if (middleline[i] > CAMERA_W - 1)middleline[i] = CAMERA_W - 1;
+			if (middleline[i] < 0)middleline[i] = 0;
+			img[i][middleline[i]] = !img[i][middleline[i]];
+		}
+	}
 
 
-	///*---------------舵机的pid---------------------*/
-	//for (i = 10; i > 1; i--)
-	//{
-	//	average_offset[i] = average_offset[i - 1];
-	//}
-	//average_offset[1] = ((float)offset / (float)count);
-	//average_offset[1] -= CAMERA_HARDWARE_ERROR;
+	/*---------------舵机的pid---------------------*/
+	for (i = 10; i > 1; i--)
+	{
+		average_offset[i] = average_offset[i - 1];
+	}
+	average_offset[1] = ((float)offset / (float)count);
+	average_offset[1] -= CAMERA_HARDWARE_ERROR;
 }
 
 
